10372144204a137a03014a2cb10879ce65fe6ac3
MariTG007 <mariitm9@gmail.com>
2022-02-27 17:43:07 -0500

    Work Complete

--- Supermarket/Supermarket/ComposedProduct.cs
+++ Supermarket/Supermarket/ComposedProduct.cs
@@ -11,30 +11,23 @@
         public override decimal ValueToPay()
         {
 
-            decimal payroll = 0;
+            decimal amount = 0;
             foreach (Product product in Products)
             {
 
-                payroll += product.ValueToPay() - (product.ValueToPay() * (decimal)Discount);
+                amount += product.ValueToPay() - (product.ValueToPay() * (decimal)Discount);
             }
 
-            return payroll;
+            return amount;
         }
         public override string ToString()
 
         {
-
-           
             string product = "";
             foreach (Product prod in Products)
             {
-                product += prod.Description + ", ";
-             
+                product += prod.Description + ", ";  
             }
-
-
-
-            
             return $" {Id} - {Description} " +
                 $"\n\tProducts...: {$"{product.Replace("Cabano, ", "Cabano")}",18}" +
                 $"\n\tDiscount...:  {$"{Discount:P2}",18}" +
